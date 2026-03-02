5c4ba8de38b0ac10f4f53cd51ef46913a48104c8
Your friendly neighborhood programmer <colombo.jordan@franchettisalviani.net>
2026-03-02 08:58:27 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace hanged_man
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sports = {"calcio","pallacanestro","giavellotto"};
+            string[] war = {"soldato","drone","icbm"};
+            string[] animals={"cane","cinghiale","scarafaggio" };
+            int difficulty = 0;
+            string chosen_word= "";
+            Console.WriteLine("insert difficulty ");
+            difficulty = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+}
